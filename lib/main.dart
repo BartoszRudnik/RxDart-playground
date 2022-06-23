1a57a23fd8c90eca586ed923c3c6c068c576602a
BartoszRudnik <rudnik49@gmail.com>
2022-06-23 11:36:19 +0200

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:rx_dart/home_page.dart';
+import 'package:rxdart/rxdart.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+void testIt() async {
+  final stream1 = Stream.periodic(
+    const Duration(
+      seconds: 1,
+    ),
+    (count) => 'Stream 1: $count',
+  ).take(3);
+
+  final stream2 = Stream.periodic(
+    const Duration(
+      seconds: 3,
+    ),
+    (count) => 'Stream 2: $count',
+  );
+
+  final result = stream1.concatWith(
+    [
+      stream2,
+    ],
+  );
+
+  await for (final value in result) {
+    print(value);
+  }
 }
 
 class MyApp extends StatelessWidget {
