83396a8b6f6a760ecf270f1794ff067826e0d8c4
BartoszRudnik <rudnik49@gmail.com>
2022-06-23 13:01:14 +0200

    step-7

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final BehaviorSubject<DateTime> subject;
+  late final Stream<String> streamOfStrings;
+
+  @override
+  void initState() {
+    super.initState();
+
+    subject = BehaviorSubject<DateTime>();
+    streamOfStrings = subject.switchMap(
+      (dateTime) => Stream.periodic(
+        const Duration(
+          seconds: 1,
+        ),
+        (value) => 'Count $value, datetime $dateTime',
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    subject.close();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          StreamBuilder<String>(
+            stream: streamOfStrings,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(
+                  snapshot.requireData,
+                );
+              } else {
+                return const Text(
+                  'Waiting for data',
+                );
+              }
+            },
+          ),
+          TextButton(
+            onPressed: () {
+              subject.add(
+                DateTime.now(),
+              );
+            },
+            child: const Text(
+              'Start stream',
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:rx_dart/home_page.dart';
-import 'package:rxdart/rxdart.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-void testIt() async {
-  final stream1 = Stream.periodic(
-    const Duration(
-      seconds: 1,
-    ),
-    (count) => 'Stream 1: $count',
-  );
-
-  final stream2 = Stream.periodic(
-    const Duration(
-      seconds: 3,
-    ),
-    (count) => 'Stream 2: $count',
-  );
-
-  final result = Rx.zip2(
-    stream1,
-    stream2,
-    (a, b) => 'A is $a, B is $b',
-  );
-
-  await for (final value in result) {
-    print(value);
-  }
 }
 
 class MyApp extends StatelessWidget {
