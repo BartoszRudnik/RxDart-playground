aed4c6eda77a027f97958a01219ea36525b7600b
BartoszRudnik <rudnik49@gmail.com>
2022-06-23 14:42:32 +0200

    step-8

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 
+enum TypeOfThing {
+  animal,
+  person,
+}
+
+@immutable
+class Thing {
+  final TypeOfThing typeOfThing;
+  final String name;
+
+  const Thing({
+    required this.typeOfThing,
+    required this.name,
+  });
+}
+
+const things = [
+  Thing(
+    typeOfThing: TypeOfThing.person,
+    name: "Foo",
+  ),
+  Thing(
+    typeOfThing: TypeOfThing.person,
+    name: 'Bar',
+  ),
+  Thing(
+    typeOfThing: TypeOfThing.animal,
+    name: 'Bunz',
+  ),
+];
+
+@immutable
+class Bloc {
+  final Sink<TypeOfThing?> setTypeOfThing;
+  final Stream<TypeOfThing?> currentTypeOfThing;
+  final Stream<Iterable<Thing>> things;
+
+  void dispose() {
+    setTypeOfThing.close();
+  }
+
+  const Bloc._({
+    required this.currentTypeOfThing,
+    required this.setTypeOfThing,
+    required this.things,
+  });
+
+  factory Bloc({
+    required Iterable<Thing> things,
+  }) {
+    final typeOfThingSubject = BehaviorSubject<TypeOfThing?>();
+    final filteredThings = typeOfThingSubject
+        .debounceTime(
+      const Duration(
+        milliseconds: 300,
+      ),
+    )
+        .map(
+      (typeOfThing) {
+        if (typeOfThing != null) {
+          return things.where(
+            (thing) => thing.typeOfThing == typeOfThing,
+          );
+        } else {
+          return things;
+        }
+      },
+    ).startWith(
+      things,
+    );
+
+    return Bloc._(
+      currentTypeOfThing: typeOfThingSubject.stream,
+      setTypeOfThing: typeOfThingSubject.sink,
+      things: filteredThings,
+    );
+  }
+}
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -9,58 +88,67 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late final BehaviorSubject<DateTime> subject;
-  late final Stream<String> streamOfStrings;
+  late final Bloc bloc;
 
   @override
   void initState() {
     super.initState();
 
-    subject = BehaviorSubject<DateTime>();
-    streamOfStrings = subject.switchMap(
-      (dateTime) => Stream.periodic(
-        const Duration(
-          seconds: 1,
-        ),
-        (value) => 'Count $value, datetime $dateTime',
-      ),
+    bloc = Bloc(
+      things: things,
     );
   }
 
   @override
   void dispose() {
-    subject.close();
-
+    bloc.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Column(
         children: [
-          StreamBuilder<String>(
-            stream: streamOfStrings,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(
-                  snapshot.requireData,
-                );
-              } else {
-                return const Text(
-                  'Waiting for data',
-                );
-              }
-            },
-          ),
-          TextButton(
-            onPressed: () {
-              subject.add(
-                DateTime.now(),
+          StreamBuilder<TypeOfThing?>(
+            stream: bloc.currentTypeOfThing,
+            builder: (ctx, snapshot) {
+              final selectedTypeOfThing = snapshot.data;
+
+              return Wrap(
+                children: TypeOfThing.values.map(
+                  (typeOfThing) {
+                    return FilterChip(
+                      selectedColor: Colors.blueAccent,
+                      label: Text(typeOfThing.name),
+                      selected: selectedTypeOfThing == typeOfThing,
+                      onSelected: (selected) {
+                        final type = selected ? typeOfThing : null;
+
+                        bloc.setTypeOfThing.add(type);
+                      },
+                    );
+                  },
+                ).toList(),
               );
             },
-            child: const Text(
-              'Start stream',
+          ),
+          Expanded(
+            child: StreamBuilder<Iterable<Thing>>(
+              stream: bloc.things,
+              builder: (context, snapshot) {
+                final things = snapshot.data ?? [];
+
+                return ListView.builder(
+                  itemCount: things.length,
+                  itemBuilder: (ctx, index) {
+                    return Text(
+                      things.elementAt(index).name + " " + things.elementAt(index).typeOfThing.name,
+                    );
+                  },
+                );
+              },
             ),
           ),
         ],
